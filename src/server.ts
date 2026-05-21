3f895e9016ba1f1ec754fc9a25c1c46ef8ed5b03
AHMED <ahmedtexting@gmail.com>
2026-05-22 03:32:58 +0600

    route handling with http

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req);
-})
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    // console.log(req.method);
+    // console.log(req.url);
+    const url = req.url
+    const method = req.method
+
+    if (url == '/' && method == 'GET') {
+        res.writeHead(200, { "content-type": 'application/json' });
+        res.end(JSON.stringify({ message: 'this is root route...' }))
+    } else if (url?.startsWith('/products')) {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ message: 'this is product route' }))
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Route Not Found' }))
+    }
+}
+);
 
 
 server.listen(5000, () => {
