f25bd534db0e7b67989e86d6f88001628a5b04f1
AHMED <ahmedtexting@gmail.com>
2026-05-23 01:02:34 +0600

    build get method to get single product

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,14 +1,26 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service";
+import type { Iproduct } from "../types/product.type";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
     const product = readProduct();
 
+    const urlParts = url?.split('/');
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null
+    console.log(id);
+
     if (url === '/products' && method === 'GET') {
         res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify({ message: 'this is product route', data: product }))
+        res.end(JSON.stringify({ message: 'Product Retrieved Successfully', data: product }))
+    } else if (method === "GET" && id !== null) {
+        const products = readProduct();
+        const product = products.find((p:Iproduct)=> p.id === id)
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Product Retrieved Successfully', data: product }))
+        console.log(product);
+
     }
 
 }
